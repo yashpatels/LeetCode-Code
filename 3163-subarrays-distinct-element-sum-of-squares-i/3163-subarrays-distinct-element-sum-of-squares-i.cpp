9b9917a3f95c3970df4905f04ce8f3eeb9977ce2
yashpatels <yash109916@gmail.com>
2023-10-29 15:58:02 -0400

    Time: 877 ms (10%), Space: 312.9 MB (10%) - LeetHub

--- 3163-subarrays-distinct-element-sum-of-squares-i/3163-subarrays-distinct-element-sum-of-squares-i.cpp
+++ 3163-subarrays-distinct-element-sum-of-squares-i/3163-subarrays-distinct-element-sum-of-squares-i.cpp
@@ -6,12 +6,12 @@ public:
         int ans =0;
         while (current!=nums.size()+1){
             for (int i=0; i<=nums.size()-current; i++){
-                cout << i << " to " << i+current << endl;
+                // cout << i << " to " << i+current << endl;
                 std::set<int> sa(nums.begin()+i, nums.begin()+i+current);
                 // std::cout << sa.size() << std::endl;
                 ans+=(sa.size()*sa.size());
             }
-            cout << endl;
+            // cout << endl;
             current++;
         }
         return ans;
