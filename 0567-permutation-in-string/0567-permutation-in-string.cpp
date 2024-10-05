3f13501627b42cd4d1f45ce7544f0c2ce8d8fa27
yashpatels <yash109916@gmail.com>
2024-10-05 14:15:39 -0400

    Time: 17 ms (26.57%), Space: 12.8 MB (18.65%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+       map<char, int> m;
+
+       for (char x: s1) {
+        m[x]++;
+       } 
+
+       int left=0;
+       int right=0;
+        map<char, int> m1;
+       for (int right=0; right<s2.size(); right++) {
+            if (m.find(s2[right]) != m.end()) {
+                //add to map;
+                m1[s2[right]]++;
+
+                while (right - left + 1 > s1.size()) {
+                    if (m1[s2[left]] == 1) {
+                        m1.erase(s2[left]);
+                    } else {
+                        m1[s2[left]]--;
+                    }
+                    left++;
+                }
+                //check map are equals;
+                if (m1 == m) {
+                    return true;
+                }
+            } else {
+                left=right+1;
+                m1.clear();
+            }
+       }
+       for (auto x: m1) {
+        cout << x.first << " " << x.second << endl;
+       }
+       if (m1==m) return true;
+       return false;
+    }
+};
