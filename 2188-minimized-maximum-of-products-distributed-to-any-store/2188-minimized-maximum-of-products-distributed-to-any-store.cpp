46b079ec8b4e12acfcedb0d17df2d599d35755a7
yashpatels <yash109916@gmail.com>
2024-11-14 00:51:14 -0500

    Time: 21 ms (93.63%), Space: 87.3 MB (72.13%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int canDistribute(int k, vector<int>& quantities) {
+        int current=0;
+
+        for (auto x: quantities) {
+            if (x%k!=0) current++;
+            current+=x/k;
+        }
+        // cout << "for " << k << " " << current << endl;
+        return current;
+    }
+
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int start=1;
+        int end=INT_MIN;
+        for (auto x: quantities) {
+            end=max(end, x);
+        }
+        int ans = INT_MAX;
+
+        while(start<=end) {
+            int middle = start+(end-start)/2;
+            int current = canDistribute(middle, quantities);
+
+            if (current < n) {
+                ans=min(middle, ans);
+                end=middle-1;
+            } else if (current > n) {
+                // ans=min(middle, ans);
+                start=middle+1;
+            } else {
+                ans=min(middle, ans);
+                end=middle-1;
+            }
+        }
+        return ans;
+    }
+};
